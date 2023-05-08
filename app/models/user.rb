4d7a741927c76907b7d6cb40c6fa32f60f48fd79
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-05-08 02:45:14 +0000

    [update]応用課題2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :books
   has_one_attached :profile_image
 
-  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: tr
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50}
 
 
