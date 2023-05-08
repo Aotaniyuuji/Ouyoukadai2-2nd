f7db2dbe711b74f840bc728c338c6865ae70a0fd
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-05-08 03:10:44 +0000

    [update]応用課題2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :is_matching_login_user
+  before_action :is_matching_login_user, only: [:edit,:update,:update]
 
   def show
     @book = Book.find(params[:id])
