5a1166c363cb2a98fb190a966b6996d30d4639bf
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-05-08 04:10:25 +0000

    [update]応用課題3

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,6 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:boo_id])
+    book = Book.find(params[:book_id])
     favorite = current_user.favorite.new(book_id: book.id)
     favorite.save
     redirect_to request.referer
